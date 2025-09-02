f9aa90406cace0961f0d87a272b7746e035236af
ommanuel <eonojefemue@bluetag.tech>
2025-09-02 22:04:23 +0100

    updated route

---
+++ backend/routes/auth.route.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const { registerUser, loginUser, getUserProfile } = require("../controllers/auth.controller");
+const { protect } = require("../middlewares/auth.middleware");
+const upload = require("../middlewares/upload.middleware");
+
+const router = express.Router();
+
+// Auth Routes
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/profile", protect, getUserProfile);
+
+router.post("/upload-image", upload.single("image"), (req, res) => {
+    console.log("upload routes");
+    if (!req.file) {
+        return res.status(400).json({ message: "No file uploaded" });
+    }
+    const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${
+        req.file.filename
+    }`;
+    res.status(200).json({ imageUrl });
+})
+
+module.exports = router;
